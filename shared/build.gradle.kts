69a2400583e83edd48647eb48de2c6c379d1c4b0
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2024-03-03 16:06:57 +0100

    Add SQL Delight and Ktor dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -22,13 +22,31 @@ kotlin {
             isStatic = true
         }
     }
-
+    val coroutinesVersion = "1.7.3"
+    val ktorVersion = "2.3.7"
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.1"
     sourceSets {
+
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+            implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+        }
+
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
         }
     }
 }
