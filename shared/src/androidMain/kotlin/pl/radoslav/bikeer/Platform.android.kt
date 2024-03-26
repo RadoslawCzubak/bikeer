03d2968a0945121ee3025493b1eb3ef3f3b05675
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2024-03-26 23:19:44 +0100

    Remove unused files

--- shared/src/androidMain/kotlin/pl/radoslav/bikeer/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package pl.radoslav.bikeer
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/pl/radoslav/bikeer/Platform.kt
+++
@@ -1,7 +0,0 @@
-package pl.radoslav.bikeer
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
